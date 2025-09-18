b3e819e172ab7e824a6c9bf134b733a0b94104a2
Julia de Altino <julia.correa.souza@gmail.com>
2025-09-18 10:21:26 -0300

    adicionado formulário em chamados

--- front2/src/routes/Chamados/index.tsx
+++ front2/src/routes/Chamados/index.tsx
@@ -2,34 +2,34 @@ export default function Chamados(){
     return(
         <main>
             <h1>Abra um Ticket</h1>
-        {/* <section class="formulario">
-            <form id="formularioTicket" action="#" method="post">
-                <label for="nome">Nome</label><br>
-                <input type="text" id="nome" name="nome" required><br>
-                  
-                <label for="email">Email</label><br>
-                <input type="email" id="email" name="email" required><br>
-                  
-                <label for="telefone">Telefone</label><br>
-                <input type="tel" id="telefone" name="telefone" required><br>
-                
-                <label for="tipo">Quem você é</label><br>
-                <select id="tipo" name="tipo" required>
-                  <option value="">Selecione</option>
-                  <option value="parente">Parente</option>
-                  <option value="cuidador">Cuidador</option>
-                  <option value="paciente">Paciente</option>
-                </select><br>
-                
-                <label for="titulo">Título do Problema:</label><br>
-                <input type="text" id="titulo" name="titulo" required><br>
-                
-                <label for="descricao">Descrição do Problema:</label><br>
-                <textarea id="descricao" name="descricao" rows="5" cols="40" required></textarea><br>
-                
-                <button id="enviarFormulario" type="submit">Enviar</button>
+            <section className="formulario">
+            <form id="formularioTicket">
+              <label htmlFor="nome">Nome</label><br />
+              <input type="text" id="nome" name="nome" required /><br />
+              
+              <label htmlFor="email">Email</label><br />
+              <input type="email" id="email" name="email" required /><br />
+              
+              <label htmlFor="telefone">Telefone</label><br />
+              <input type="tel" id="telefone" name="telefone" required /><br />
+              
+              <label htmlFor="tipo">Quem você é</label><br />
+              <select id="tipo" name="tipo" required>
+                <option value="">Selecione</option>
+                <option value="parente">Parente</option>
+                <option value="cuidador">Cuidador</option>
+                <option value="paciente">Paciente</option>
+              </select><br />
+              
+              <label htmlFor="titulo">Título do Problema:</label><br />
+              <input type="text" id="titulo" name="titulo" required /><br />
+              
+              <label htmlFor="descricao">Descrição do Problema:</label><br />
+              <textarea id="descricao" name="descricao" rows={5} cols={40} required></textarea><br />
+              
+              <button id="enviarFormulario" type="submit">Enviar</button>
             </form>
-        </section> */}
+    </section>
         </main>
     )
 }
