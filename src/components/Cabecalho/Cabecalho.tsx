92ada424e9fc468ff9c36a827c7fbb62c46488cc
Matheussansao <matheussansao@hotmail.com>
2025-09-25 23:07:18 -0300

    Aplicamos layout flexbox e padding ao container

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function Cabecalho(){
     return(
-        <header>
+        // Adicionamos as classes de layout e estilo aqui
+        <header className="bg-gray-100 p-6 flex justify-between items-center shadow-lg relative">
             <Link to={"/"}>
                 <img src={logo} alt="Logo da TeleAjuda IMREA" /> 
             </Link>
