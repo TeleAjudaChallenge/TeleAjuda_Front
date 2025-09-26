9ca9d8e01243c0e98d0eec1c40f97bcaece6f4d4
Matheussansao <matheussansao@hotmail.com>
2025-09-26 18:24:03 -0300

    Criada a página de Perguntas Frequentes

---
+++ src/routes/Faq/index.tsx
@@ -0,0 +1,10 @@
+export default function Faq(){
+    return(
+        <main>
+            <h1>Perguntas Frequentes (FAQ)</h1>
+            <section>
+                <p>Em breve, esta seção terá as perguntas e respostas mais comuns.</p>
+            </section>
+        </main>
+    )
+}
