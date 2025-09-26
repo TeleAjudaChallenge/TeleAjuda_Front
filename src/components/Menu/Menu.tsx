776bcd2e75331db1c6e43d77755bf977a3712a62
Matheussansao <matheussansao@hotmail.com>
2025-09-25 23:12:06 -0300

    Convertemos Link para NavLink e implantamos estilo de link ativo

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,25 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Menu(){
+
+    const getLinkClass = ({ isActive } : { isActive: boolean }) => {
+        const baseClasses = "font-semibold px-4 py-2 rounded-full transition-all duration-300";
+        
+        if (isActive) {
+            return `${baseClasses} bg-[color:var(--color-button)] text-black shadow-md`;
+        } else {
+            return `${baseClasses} text-black hover:text-gray-600`;
+        }
+    };
+
     return(
         <nav>
-            {/* Adicionamos as classes de estilo ao <ul> */}
             <ul className="flex items-center space-x-2 bg-white/50 backdrop-blur-sm border border-gray-900/10 rounded-full p-2">
-                <li><Link to={"/"} className="text-black hover:text-gray-600">Home</Link></li>
-                <li><Link to={"/chamados"} className="text-black hover:text-gray-600">Chamados</Link></li>
-                <li><Link to={"/contato"} className="text-black hover:text-gray-600">Contato</Link></li>
-                <li><Link to={"/pesquisa"} className="text-black hover:text-gray-600">Pesquisa</Link></li>
-                <li><Link to={"/sobre"} className="text-black hover:text-gray-600">Sobre</Link></li>
+                <li><NavLink to={"/"} className={getLinkClass}>Home</NavLink></li>
+                <li><NavLink to={"/chamados"} className={getLinkClass}>Chamados</NavLink></li>
+                <li><NavLink to={"/contato"} className={getLinkClass}>Contato</NavLink></li>
+                <li><NavLink to={"/pesquisa"} className={getLinkClass}>Pesquisa</NavLink></li>
+                <li><NavLink to={"/sobre"} className={getLinkClass}>Sobre</NavLink></li>
             </ul>
         </nav>
     )
