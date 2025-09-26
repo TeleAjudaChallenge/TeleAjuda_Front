19f01d47cd8be360625cdce1b047c102b1984541
Matheussansao <matheussansao@hotmail.com>
2025-09-25 23:15:38 -0300

    Removemos as margens internas para ajustar o layout do link ativo

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,18 +3,21 @@ import { NavLink } from "react-router-dom";
 export default function Menu(){
 
     const getLinkClass = ({ isActive } : { isActive: boolean }) => {
-        const baseClasses = "font-semibold px-4 py-2 rounded-full transition-all duration-300";
+    
+        const baseClasses = "font-semibold px-4 rounded-full transition-all duration-300";
         
+        const layoutClasses = "h-full flex items-center";
+
         if (isActive) {
-            return `${baseClasses} bg-[color:var(--color-button)] text-black shadow-md`;
+            return `${layoutClasses} ${baseClasses} bg-[color:var(--color-button)] text-black shadow-md`;
         } else {
-            return `${baseClasses} text-black hover:text-gray-600`;
+            return `${layoutClasses} ${baseClasses} text-black hover:text-gray-600`;
         }
     };
 
     return(
         <nav>
-            <ul className="flex items-center space-x-2 bg-white/50 backdrop-blur-sm border border-gray-900/10 rounded-full p-2">
+            <ul className="flex items-stretch bg-white/50 backdrop-blur-sm border border-gray-900/10 rounded-full py-1.5">
                 <li><NavLink to={"/"} className={getLinkClass}>Home</NavLink></li>
                 <li><NavLink to={"/chamados"} className={getLinkClass}>Chamados</NavLink></li>
                 <li><NavLink to={"/contato"} className={getLinkClass}>Contato</NavLink></li>
