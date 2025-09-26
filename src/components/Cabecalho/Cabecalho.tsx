95f2cf887a1aeb32c66b6e27996caf0a4b76b508
Matheussansao <matheussansao@hotmail.com>
2025-09-25 23:18:41 -0300

    Foi diminuido o tamanho da logo

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function Cabecalho(){
     return(
-        // Adicionamos as classes de layout e estilo aqui
         <header className="bg-gray-100 p-6 flex justify-between items-center shadow-lg relative">
             <Link to={"/"}>
-                <img src={logo} alt="Logo da TeleAjuda IMREA" /> 
+                {/* Adicionamos a classe h-24 para aumentar a altura do logo */}
+                <img src={logo} alt="Logo da TeleAjuda IMREA" className="h-24" /> 
             </Link>
             <Menu/>
         </header>
