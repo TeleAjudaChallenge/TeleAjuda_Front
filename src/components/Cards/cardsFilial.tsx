faa5e963b399a41885d483fa3bb99f46a40c0bc9
Matheussansao <matheussansao@hotmail.com>
2025-09-30 21:37:55 -0300

    Estilização dos componente CardFilial para o tema claro

--- src/components/Cards/cardsFilial.tsx
+++ src/components/Cards/cardsFilial.tsx
@@ -2,12 +2,18 @@ import type { FilialProps } from "../../types/filialProps.ts";
 
 export default function CardFilial({ filial }: FilialProps) {
   return (
-    
-    <article style={{border:"2px solid #000", backgroundColor:"#1b1b1b",color:"#fff", padding:"1%",margin:"2%", width:"25vw",height:"25vh"}}>
-      <h2>{filial.nome}</h2>
-      <p>Endereço: {filial.endereco}</p>
-      <p>Horário de Atendimento: {filial.horario}</p>
+    <article className="bg-white border border-gray-200 rounded-2xl p-6 shadow-lg w-full md:w-auto md:min-w-[300px]">
+      <h2 className="text-xl font-bold" style={{ color: 'var(--color-primary)' }}>
+        {filial.nome}
+      </h2>
+      <div className="mt-4 space-y-2 text-gray-600">
+        <p>
+          <strong>Endereço:</strong> {filial.endereco}
+        </p>
+        <p>
+          <strong>Horário de Atendimento:</strong> {filial.horario}
+        </p>
+      </div>
     </article>
- 
   );
 }
