bd10e1169a8514a1f6269d683b55b8bc41d319db
MatheusSansao <Matheussansao@hotmail.com>
2025-11-09 17:33:06 -0300

    feat(router): configura rotas da aplicação com layouts públicos e protegidos

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.tsx'
-import Home from "./routes/Home/index.tsx"
 import Error from "./routes/Error/index.tsx"
+
+import Home from "./routes/Home/index.tsx"
 import Chamados from "./routes/Chamados/index.tsx"
 import Contato from "./routes/Contato/index.tsx"
 import Pesquisa from "./routes/Pesquisa/index.tsx"
@@ -12,27 +15,47 @@ import Sobre from "./routes/Sobre/index.tsx"
 import Cadastro from "./routes/Cadastro/index.tsx"
 import Login from "./routes/Login/index.tsx"
 import Perfil from "./routes/Perfil/index.tsx"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import ProtectedRoute from './routes/ProtectedRoute/index.tsx';
+
+import DashboardFuncionario from "./routes/DashboardFuncionario/index.tsx";
+import VerPesquisas from "./routes/VerPesquisa/index.tsx";
+
 import "./global.css"
 
 const router = createBrowserRouter([
-  {path:"/", element:<App/>, errorElement:<Error/> , children:[
-    {path:"/",element:<Home/>},
-    {path:"/chamados",element:<Chamados/>},
-    {path:"/contato",element:<Contato/>},
-    {path:"/contato/:filialId", element:<Contato/>},
-    {path:"/pesquisa", element:<Pesquisa/>},
-    {path:"/integrantes", element:<Integrantes/>},
-    {path:"/faq",element:<Faq/>},
-    {path:"/sobre",element:<Sobre/>},
-    {path:"/cadastro",element:<Cadastro/>},
-    {path:"/login",element:<Login/>},
-    {path:"/perfil",element:<Perfil/>}
-  ]}
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+     
+      { path: "/login", element: <Login /> },
+      { path: "/cadastro", element: <Cadastro /> },
+
+      {
+        element: <ProtectedRoute />,
+        children: [
+          { path: "/", element: <Home /> },
+          { path: "/chamados", element: <Chamados /> },
+          { path: "/contato", element: <Contato /> },
+          { path: "/contato/:filialId", element: <Contato /> },
+          { path: "/pesquisa", element: <Pesquisa /> },
+          { path: "/integrantes", element: <Integrantes /> },
+          { path: "/faq", element: <Faq /> },
+          { path: "/sobre", element: <Sobre /> },
+          { path: "/perfil", element: <Perfil /> },
+         
+          { path: "/dashboard-funcionario", element: <DashboardFuncionario /> },
+          { path: "/ver-pesquisas", element: <VerPesquisas /> }
+        ]
+      }
+    ]
+  }
 ]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
