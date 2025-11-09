620792f86271d28b52b361effe584a13fcadeb28
MatheusSansao <Matheussansao@hotmail.com>
2025-11-09 17:36:43 -0300

    feat(ui): cria o componente Cabecalho reutilizável

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,12 @@
 import Menu from "../Menu/Menu";
-import logo from "../../../public/LogoTeleAjuda.png";
+import logo from "/LogoTeleAjuda.png";
 import { Link } from "react-router-dom";
 
 export default function Cabecalho(){
     return(
         <header className="bg-gray-100 p-6 flex justify-between items-center shadow-lg relative sticky top-0 z-50">
             <Link to={"/"}>
-                <img src={logo} alt="Logo da TeleAjuda" className="h-24" /> 
+                <img src={logo} alt="Logo da TeleAjuda" className="h-24" />
             </Link>
             <Menu/>
         </header>
